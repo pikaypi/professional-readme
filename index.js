a0c171bb0bb5f5b97eac3a27121ce6e8e5f154ef
Paul Pinard <paul.k.pinard@gmail.com>
2022-10-05 02:48:01 -0400

    init()

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -14,7 +15,12 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((res) => generateMarkdown(res))
+        .then((res) => writeToFile('README.md', res))
+};
 
 // Function call to initialize app
 init();
