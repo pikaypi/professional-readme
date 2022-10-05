13fe487358ce3f2c98c9fb4aae405e38decac433
Paul Pinard <paul.k.pinard@gmail.com>
2022-10-05 02:59:08 -0400

    add title to prompt

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'text',
+        message: 'What is the title of the project?',
+        name: 'title'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
