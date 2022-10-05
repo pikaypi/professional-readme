185292f0a6f23fbc9a72f395bc00c0cb08da889f
Paul Pinard <paul.k.pinard@gmail.com>
2022-10-05 02:41:32 -0400

    writeToFile

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log(`Successfully created ${fileName}!`)
+  );
+}
 
 // TODO: Create a function to initialize app
 function init() {}
