b470764e72633d4d62f41caa0587f0698f461876
Paul Pinard <paul.k.pinard@gmail.com>
2022-10-05 23:57:19 -0400

    add license prompt

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ const questions = [
         type: 'text',
         message: 'Describe the tests for the project',
         name: 'tests'
+    },
+    {
+        type: 'list',
+        message: 'Which GitHub license does the project have?',
+        name: 'license',
+        choices: ['MIT License', 'Apache 2.0', 'ISC License', 'BSD 3-Clause', 'BSD 2-Clause', 'Creative Commons Zero', 'GNU GPL v3']
     }
 ];
 
