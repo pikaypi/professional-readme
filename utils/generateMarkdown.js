8bf7e20e1038a76e81f3ef6bf5305147b5c794c9
Paul Pinard <paul.k.pinard@gmail.com>
2022-10-05 23:56:58 -0400

    render license badges with links to the license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,35 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT License':
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT`;
+
+    case 'Apache 2.0':
+      return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+    
+    case 'ISC License':
+      return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`;
+
+    case 'BSD 3-Clause':
+      return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+
+    case 'BSD 2-Clause':
+      return `[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`;      
+      
+    case 'Creative Commons Zero':
+      return `[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)`;
+      
+    case 'GNU GPL v3':
+      return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+    
+    case 'Eclipse Public License 1.0':
+      return `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`;
+                                          
+    default:
+      return ''
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -41,7 +70,7 @@ ${data.contributing}
 ## Tests
 ${data.tests}
 
----
+${renderLicenseBadge(data.license)}
 `;
 }
 
